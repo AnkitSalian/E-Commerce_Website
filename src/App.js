cb2335cc3935589eab0bfbe0d8cf7cf0e69afacd
Ankit <salian.ankit@tcs.com>
2019-08-26 07:07:12 +0530

    skeleton of homepage is designed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {HomePage} from './components/homepage/homepage.components';
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage />
     </div>
   );
 }
