3c3c641963ffc9008a57625b57eb78687f691888
Ankit <salian.ankit@tcs.com>
2019-08-27 07:02:16 +0530

    Components of homepage has been created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {HomePage} from './components/homepage/homepage.components';
+import {HomePage} from './pages/homepage/homepage.components';
 
 function App() {
   return (
