82ef91f687b9196fa5e1006f3e33266cb387e84f
Ankit <salian.ankit@tcs.com>
2019-09-03 07:18:54 +0530

    saved google auth user in firestore and created a component for signup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {HomePage} from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUp from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -21,10 +21,20 @@ class App extends React.Component {
   unSubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser : user});
-      console.log('user===>',user);
-      
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapshot=>{
+          this.setState({
+            currentUser:{
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      }
+      this.setState({currentUser : userAuth});
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
   appId: "1:859188790327:web:96e2542041382495"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData)=>{
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+  console.log(snapshot);
+
+  if(!snapshot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }catch(error){
+      console.log('Error creating user===>',error);
+    }
+  }
+  return userRef;
+  
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
