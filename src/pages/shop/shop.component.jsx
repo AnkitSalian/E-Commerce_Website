5c2113fc51ef643581afa5f89b437de41ba5ccdf
Ankit <salian.ankit@tcs.com>
2019-09-12 05:55:44 +0530

    Moved directory and shop data into reducer, created respective selectors and updated corresponding components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 
 import CollectioOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../../pages/collection/collection.component';
 
-const ShopPage = ({collections})=>{
+const ShopPage = ({match})=>{
     
         return(
             <div className="shop-page">
-                <CollectioOverview />
+                <Route exact path={`${match.path}`} component={CollectioOverview}  />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
             </div>
         );
 }
