3771ed9a0dcea71e76396622185c0f656cb5209d
Ankit <salian.ankit@tcs.com>
2019-10-29 06:20:24 +0530

    Handle stripe from a express server within the app

--- client/src/components/stripe/stripe.component.jsx
+++ client/src/components/stripe/stripe.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishKey = "pk_test_NVUlkADzo4fnJsq425ezLm7x00tL4D7JBI";
 
     const onToken = (token)=>{
-        console.log(token);
-        alert('Paymemt Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful')
+        }).catch(error => {
+            console.log('Payment error: ',JSON.parse(error));
+            alert('There was an issue with your payment. Please make sure you are using the provided credit card')
+        })
     }
 
     return (
