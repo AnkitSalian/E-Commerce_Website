8db9c51dd3a229c2947075055fccbe7d9c9379ac
Ankit <salian.ankit@tcs.com>
2019-11-13 06:58:25 +0530

    Mobile compatability changes done for app padding globally

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {Switch ,Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
-import './App.css';
+import {GloballStyle} from './global.styles';
 
 import {HomePage} from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component';
@@ -22,6 +22,7 @@ const App = ({checkUserSession, currentUser}) => {
   
     return (
       <div className="App">
+        <GloballStyle />
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
